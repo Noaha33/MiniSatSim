d28de35ed1aeb7e8cb1e0f2f2158ec664b5b6903
Noaha33 <noaha33@vt.edu>
2026-05-19 02:01:11 -0600

    Update tlm.py to check for data

--- telemetry.py
+++ telemetry.py
@@ -1,7 +1,9 @@
+from pathlib import Path
 import numpy as np
 
-
 def save_history_to_csv(filename, history_array):
+    Path(filename).parent.mkdir(parents=True, exist_ok=True)
+
     header = "time_s,x_km,y_km,z_km,vx_km_s,vy_km_s,vz_km_s,altitude_km,speed_km_s"
 
     np.savetxt(
